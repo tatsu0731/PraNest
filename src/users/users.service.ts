5fba93b42c527593acad08a0ee8af24752a24cbf
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-10-17 08:27:02 +0900

    DB接続は完了

---
+++ src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create({ name }: CreateUserDto): Promise<User> {
+    return await this.userRepository.save({ name: name }).catch((e) => {
+      throw new InternalServerErrorException(
+        `[${e.message}]:ユーザーの登録に失敗しました。`,
+      );
+    });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
